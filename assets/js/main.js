9b04d74cba0efd63e7f648f10b8baaa5a22d2e5b
Vitaliy <53347040+Vitaliy-afk@users.noreply.github.com>
2020-06-21 20:57:51 +0300

    last

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,32 @@
 const spans = document.querySelectorAll('h1 span')
-spans.forEach(span => span.addEventListener('mouseover', function() {
+spans.forEach(span => span.addEventListener('mouseover', function(e) {
     span.classList.add('animated', 'rubberBand')
 }))
-spans.forEach(span => span.addEventListener('mouseout', function() {
+spans.forEach(span => span.addEventListener('mouseout', function(e) {
     span.classList.remove('animated', 'rubberBand')
 }))
 
 
 
-// const htmlBar = document.querySelector('.bar-html')
-// const cssBar = document.querySelector('.bar-css')
-// const jsBar = document.querySelector('.bar-javascript')
-// const reactBar = document.querySelector('.bar-react')
+const htmlBar = document.querySelector('.bar-html')
+const cssBar = document.querySelector('.bar-css')
+const jsBar = document.querySelector('.bar-javascript')
+const reactBar = document.querySelector('.bar-react')
 
-// var t1 = new TimelineLite()
+var t1 = new TimelineLite()
 
-// t1.fromTo(htmlBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
-//     .fromTo(cssBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(95% - 6px)', ease: Power4.easeOut})
-//     .fromTo(jsBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(75% - 6px)', ease: Power4.easeOut})
-//     .fromTo(reactBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(70% - 6px)', ease: Power4.easeOut})
+t1.fromTo(htmlBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
+    .fromTo(cssBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(95% - 6px)', ease: Power4.easeOut})
+    .fromTo(jsBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(75% - 6px)', ease: Power4.easeOut})
+    .fromTo(reactBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(70% - 6px)', ease: Power4.easeOut})
 
-// const controller = new ScrollMagic.Controller()
-// const scene = new ScrollMagic.Scene({
-//     triggerElement: '.skills',
-//     triggerHook: 0
-// })
-// .setTween(t1)
-// .addTo(controller)
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.skills',
+    triggerHook: 0
+})
+.setTween(t1)
+.addTo(controller)
 
 
 const showRequiredCategory = event => {
@@ -41,7 +41,7 @@ const showRequiredCategory = event => {
     event.classList.add('active')
     const getCategory = document.querySelector('.category-${getId}')
     const categories = document.querySelectorAll('div[class ^= "category-"]')
-    for(i=0; i<categories.length; i++) {
+    for(i=0; i<categories.length; i++){
         if(categories[i].hasAttribute('class')) {
             categories[i].classList.remove('showCategory')
             categories[i].classList.add('hideCategory')
