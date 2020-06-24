69f40b7f2a3c8ce7004d78c67cfc6a2cd6bde25b
Vitaliy <53347040+Vitaliy-afk@users.noreply.github.com>
2020-06-24 22:13:10 +0300

    last

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ spans.forEach(span => span.addEventListener('mouseout', function(e) {
 }))
 
 
-
 const htmlBar = document.querySelector('.bar-html')
 const cssBar = document.querySelector('.bar-css')
 const jsBar = document.querySelector('.bar-javascript')
@@ -15,41 +14,43 @@ const reactBar = document.querySelector('.bar-react')
 
 var t1 = new TimelineLite()
 
-t1.fromTo(htmlBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
-    .fromTo(cssBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(95% - 6px)', ease: Power4.easeOut})
-    .fromTo(jsBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(75% - 6px)', ease: Power4.easeOut})
-    .fromTo(reactBar, .75, {width: 'calc(0% - 6px)'}, {width: 'calc(70% - 6px)', ease: Power4.easeOut})
+t1.fromTo(htmlBar, .50, {width: 'calc(0% - 6px)'}, {width: 'calc(75% - 6px)', ease: Power4.easeOut})
+    .fromTo(cssBar, .50, {width: 'calc(0% - 6px)'}, {width: 'calc(70% - 6px)', ease: Power4.easeOut})
+    .fromTo(jsBar, .50, {width: 'calc(0% - 6px)'}, {width: 'calc(75% - 6px)', ease: Power4.easeOut})
+    .fromTo(reactBar, .50, {width: 'calc(0% - 6px)'}, {width: 'calc(70% - 6px)', ease: Power4.easeOut})
 
 const controller = new ScrollMagic.Controller()
 const scene = new ScrollMagic.Scene({
     triggerElement: '.skills',
     triggerHook: 0
 })
+
+
 .setTween(t1)
 .addTo(controller)
 
 
-const showRequiredCategory = event => {
-    const getId = event.id
-    const links = document.querySelectorAll('.work-category button')
-    for(i=0; i<links.length; i++) {
-        if(links[i].hasAttribute('class')) {
-            links[i].classList.remove('active')
+function showRequiredCategory(event) {
+    const getId = event.id;
+    const links = document.querySelectorAll('.work-category button');
+    for (i = 0; i < links.length; i++) {
+        if (links[i].hasAttribute('class')) {
+            links[i].classList.remove('active');
         }
     }
 
-    event.classList.add('active')
-    const getCategory = document.querySelector('.category-${getId}')
-    const categories = document.querySelectorAll('div[class ^= "category-"]')
-    for(i=0; i<categories.length; i++){
-        if(categories[i].hasAttribute('class')) {
-            categories[i].classList.remove('showCategory')
-            categories[i].classList.add('hideCategory')
+    event.classList.add('active');
+    const getCategory = document.querySelector(`.category-${getId}`);
+    const categories = document.querySelectorAll('div[class ^= "category"]');
+    for (j = 0; j < categories.length; j++) {
+        if (categories[j].hasAttribute('class')) {
+            categories[j].classList.remove('showCategory');
+            categories[j].classList.add('hideCategory');
         }
     }
 
-    getCategory.classList.remove('hideCategory')
-    getCategory.classList.add('showCategory')
+    getCategory.classList.remove('hideCategory');
+    getCategory.classList.add('showCategory');
 }
 
 console.log(2234);
